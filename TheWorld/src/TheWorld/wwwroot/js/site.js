101e4c4a6355764a3800ad7662ac447bf0ee6051
Kevin Archibald <karchibald@gmail.com>
2016-12-01 15:02:35 -0500

    enter the java

---
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿//site.js
+(function () {
+var ele = document.getElementById("username");
+ele.innerHTML = "Kevin B. Archibald";
+
+var main = document.getElementById("main");
+
+main.onmouseenter = function () {
+    main.style.backgroundColor = "#888";
+};
+
+main.onmouseleave = function () {
+    main.style.backgroundColor = "";
+};
+})(); 
+
