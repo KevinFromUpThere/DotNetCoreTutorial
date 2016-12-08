0839e5de0040b4e4a57f9e13e1070875bf5cda21
Kevin Archibald <karchibald@gmail.com>
2016-12-08 16:05:38 -0500

    layout

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -3,17 +3,41 @@
 
  
 
-var ele = $("#username");
-ele.text("Kevin B. Archibald");
+//var ele = $("#username");
+//ele.text("Kevin B. Archibald");
 
-var main = $("main");
+//var main = $("main");
 
-main.on("mouseenter", function () {
-    main.style.backgroundColor = "#888";
-});
+//main.on("mouseenter", function () {
+//    main.style.backgroundColor = "#888";
+//});
 
-    main.on("mouseleave", function () {
-        main.style.backgroundColor = "";
+//    main.on("mouseleave", function () {
+//        main.style.backgroundColor = "";
+//    });
+
+//    var menuitems = $("ul.menu li a");
+//    menuitems.on("click", function () {
+
+//        var me = $(this);
+//        alert(me.text());
+    //    })
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        }
+        else
+        { $(this).text("Hide Sidebar"); }
+                
+
+
     });
+
+
+
 })(); 
 
